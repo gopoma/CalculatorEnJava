64d09131a11815004b9f1b4e37f782e7a65ebc1c
Gustavo <gordono@unsa.edu.pe>
2022-05-01 17:28:56 -0500

    Se agregaron las funciones sub y div

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,11 @@
+import java.util.Random;
 class Calculator {
   int add(int a, int b) { return 0; }
-  int sub(int a, int b) { return 0; }
+  int sub(int a, int b) { return a - b; }
   int mul(int a, int b) { return 0; }
-  int div(int a, int b) { return 0; }
+  int div(int a, int b) { return a / b; }
   int mod(int a, int b) { return 0; }
-}  
+  public static void main(String[] args) {
+    System.out.println(new Random().nextInt(2));
+  }
+}
